9e2d4b42f8e4b1adbfb34b62ff2438aa211a9b73
Owen McMonagle <owenmcmonagle@ymail.com>
2017-02-02 22:18:51 +0000

    Update Swap.java

--- Swap.java
+++ Swap.java
@@ -5,20 +5,26 @@ public class Swap
 	{
 		// Instantiate variables to swap.
 		int a = 0, b = 1; 
+		
+		
 		// Begin swap...
-		// Make variable 'a' equal the returned integer, place
-		// 'b' as the first parameter so 'b' is swapped to 'a'
-		// when the method returns.
-		//
-		// Using the second parameter as a placeholder, use the
-		// assignment operator to make 'b' equal 'a' before swap
-		// returns 'b' to be assigned to 'a'.
-		// 
+		// Make variable 'a' equal the returned integer.
+		a = swap( 
+				// place 'b' as the first parameter so 'b' is swapped to 'a'
+				// when the method returns.
+				b,
+				// Using the second parameter as a placeholder, use the
+				// assignment operator to make 'b' equal 'a' before swap
+				// returns 'b' to be assigned to 'a'.
+				(b = a) 
+			);
+		
 		// This is a compiler trick that wouldn't ordinarily come 
 		// to mind even though it's so simple. While I don't think
 		// it has any production value, it definitely is a nice 
 		// brain tease. 
-		a = swap( b, (b = a) );
+		
+		
 		// Print results.
 		System.out.println( "A:" + a + "\nB:" + b );
 	}
